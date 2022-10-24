196199a114529a2dcb78687bbfd5dd56d3b60272
Andreen-Muchiri <andreenshii@gmail.com>
2022-10-24 22:31:13 +0300

    Complete assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+class Student < User
+attr_accessor :knowledge
 
-end
+    def initialize
+           @knowledge = []
+    end
+
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
+
+   end
