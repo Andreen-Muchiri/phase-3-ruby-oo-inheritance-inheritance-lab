28f38d8429f1ac78104f840cf09b2b3f988723bd
Andreen-Muchiri <andreenshii@gmail.com>
2022-10-24 22:33:09 +0300

    Complete assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,10 @@
 class Student < User
 attr_accessor :knowledge
-
+# initialize with empty array
     def initialize
            @knowledge = []
     end
-
+# instance method,string argument, add to empty array
     def learn(knowledge)
         @knowledge << knowledge
     end
